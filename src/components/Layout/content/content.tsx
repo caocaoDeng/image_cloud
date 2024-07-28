77b87288b3f1550d682fba6fca3b7146b66f06d8
Deng`·caoaco <dengcaocao.dcc@gamil.com>
2024-07-28 11:44:01 +0800

    menu style and scorllbar style

--- src/components/Layout/content/content.tsx
+++ src/components/Layout/content/content.tsx
@@ -1,5 +1,5 @@
 import styles from './content.module.scss'
 
 export default function Content({ children }: { children: React.ReactNode }) {
-    return <div className={styles.content}>{children}</div>
+    return <div className={`${styles.content} el-scrollbar transition-all`}>{children}</div>
 }
--- src/components/Layout/sider/sider.tsx
+++ src/components/Layout/sider/sider.tsx
@@ -1,7 +1,18 @@
 import styles from './sider.module.scss'
 
 export default function Sider() {
-    return <nav className={styles.nav}>
-        
+  return (
+    <nav className={styles.nav}>
+      <ul className="h-full px-2.5 overflow-auto el-scrollbar">
+        {new Array(40).fill(0).map((_, index) => {
+            return (
+                <li key={index} className={styles['dir-item']}>
+                    <span className="iconfont icon-24gf-folder"></span>
+                    <span className="flex-1 truncate">文件夹名称{index}</span>
+                </li>
+            )
+        })}
+      </ul>
     </nav>
+  )
 }
