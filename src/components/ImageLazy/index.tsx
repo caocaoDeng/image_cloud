37b552ffafed51475861f8de8ae387830cf5015e
Deng Caocao <dengcaocao.dcc@gmail.com>
2024-08-06 16:29:54 +0800

    update image lazy

--- src/components/ImageLazy/index.tsx
+++ src/components/ImageLazy/index.tsx
@@ -1,30 +1,107 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 
+export const enum loadText {
+  LOADING = '加载中...',
+  ERROR = '加载失败',
+}
+
 export default function ImageLazy({
+  target,
   lazy = false,
+  className,
   src,
   ...rest
 }: {
-  lazy: boolean
+  target?: Window | HTMLElement | null
+  lazy?: boolean
+  className: string
   width: number
   height: number
   src: string
   alt: string
 }) {
-  const imgElm = useRef<HTMLImageElement>(null)
+  const elm = useRef<HTMLDivElement>(null)
+  const [loadSrc, setLodSrc] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(true)
+  const [text, setText] = useState<string>(loadText.LOADING)
 
-  const computedLocation = () => {
+  /**
+   * 判断是否在窗口内
+   * @param scrollTop 滚动条的位置
+   * @returns boolean
+   */
+  const computedLocation = (scrollTop: number) => {
     const windowHeight = screen.height
-    const elmTop = imgElm.current?.offsetTop
-    const scrollTop = window.scrollY
+    const { top } = elm.current?.getBoundingClientRect() as DOMRect
+    return top - scrollTop < windowHeight
+  }
+
+  /**
+   * 加载图片
+   * @returns Promise<boolean>
+   */
+  const loadImage = () => {
+    return new Promise<boolean>((res, rej) => {
+      const image = new window.Image()
+      image.src = src
+      image.onload = () => res(true)
+      image.onerror = () => rej(false)
+      image.onabort = () => rej(false)
+    })
+      .catch(() => {
+        setText(loadText.ERROR)
+      })
+      .finally(() => {
+        setLodSrc(src)
+        setLoading(false)
+        target?.removeEventListener('scroll', initLoad)
+      })
+  }
+
+  /**
+   * 加载逻辑
+   * @param e Scroll Event
+   * @returns Promise
+   */
+  const initLoad = async (e?: Event) => {
+    if (!lazy) return await loadImage()
+    // 首次加载 scrollTop 为0
+    const { scrollTop = 0 } = e ? (e.target as HTMLElement) : {}
+    const isInWindow = computedLocation(scrollTop)
+    isInWindow && (await loadImage())
+  }
+
+  // 注册监听事件
+  const installListenerEnvent = () => {
+    if (!lazy) return
+    window.removeEventListener('scroll', initLoad)
+    target?.addEventListener('scroll', initLoad)
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', (e: any) => {
-      console.log(e)
-    })
+    initLoad()
   }, [])
-  // eslint-disable-next-line jsx-a11y/alt-text
-  return <Image ref={imgElm} {...rest} src={''}></Image>
+
+  useEffect(() => {
+    installListenerEnvent()
+  }, [target])
+
+  return (
+    <div ref={elm} className={`relative inline-block ${className}`}>
+      {loading || text === loadText.ERROR ? (
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-sm text-zinc-200">
+          <p
+            className={`iconfont icon-loading1 animate-spin ${
+              text === loadText.ERROR ? 'hidden' : ''
+            }`}
+          ></p>
+          <p>{text}</p>
+        </div>
+      ) : (
+        <Image className="w-full h-full" {...rest} src={loadSrc}></Image>
+      )}
+    </div>
+  )
 }
